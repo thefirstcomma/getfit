59aacf0622e20b701abb977e2d51ba3008e3e710
Andrew Kim <commanderjacks@gmail.com>
2019-09-29 14:21:53 -0700

    Sql stuff

--- action_page.php
+++ action_page.php
@@ -1,10 +1,33 @@
 <html>
 <body>
 
+<?php
+$link = mysqli_connect("localhost", "root", "", "demo");
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+?>
+
 Welcome <?php echo $_POST["name"]; ?><br>
 Chest: <?php echo $_POST["chest"]; ?><br>
 Stomach: <?php echo $_POST["stomach"]; ?><br>
 Waist: <?php echo $_POST["waist"]; ?><br>
 
+
+<?php
+$sql = "INSERT INTO bodysize (first_name, last_name, email) VALUES ('Peter', 'Parker', 'peterparker@mail.com')";
+
+if(mysqli_query($link, $sql)){
+    echo "Records inserted successfully.";
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
+ 
+// Close connection
+mysqli_close($link);
+
+?>
 </body>
 </html>
